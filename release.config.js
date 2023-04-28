5997118e375eea253e501c2307464bec7ce4876a
Zoe Gonzales <zoegonzales03@gmail.com>
2023-04-28 14:09:27 -0600

    feat: initial commit

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ["+([0-9])?(.{+([0-9]),x}).x", "main", "next", "next-major"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        successComment:
+          ":rocket: This pull request is included in version ${nextRelease.version}.",
+        releasedLabels: [":rocket: released"],
+        publish: false,
+      },
+    ],
+  ],
+};
